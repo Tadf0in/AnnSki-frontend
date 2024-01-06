9b850de6550a7223941c3095ba3cc63eb1bdb912
Louis AMOUDRUZ <louis.amoudruz@gmail.com>
2024-01-06 13:19:11 +0100

    responsive

--- src/components/Shop/Buy.jsx
+++ src/components/Shop/Buy.jsx
@@ -16,22 +16,24 @@ export default function Buy() {
         {loading && <Loading />}    
 
         { data ? <div className="buy d-flex flex-column align-items-center">
-            <div className="buy-left">
-                <h2>{data.nom}</h2>
-                <img src={data.image} alt="photo"/>
-            </div>
+            <div className="buy-page d-flex justify-content-center">
+                <div className="buy-left">
+                    <h2>{data.nom}</h2>
+                    <img src={data.image} alt="photo"/>
+                </div>
 
-            <form className="buy-form">
-                <span>En stock : {data.stock}</span>
-                <FormFloating type="number" name="quantite" id="buy-form-quantite" placeholder="Quantité" get={formData} set={setFormData}/>
-                <span className="span-prix-buy d-flex">
-                    <span className="fw-bold fs-4 prix">{data.prix * formData.quantite}€</span>
-                    &nbsp;
-                    <button type="submit" className="btn btn-primary" 
-                        disabled={formData.quantite <=0 || formData.quantite > data.stock || (formData.quantite*1) % 1 !== 0}
-                    >Acheter</button>
-                </span>
-            </form>
+                <form className="buy-right d-flex flex-column justify-content-end">
+                    <span>En stock : {data.stock}</span>
+                    <FormFloating type="number" name="quantite" id="buy-form-quantite" placeholder="Quantité" get={formData} set={setFormData}/>
+                    <span className="span-prix-buy d-flex">
+                        <button type="submit" className="btn btn-primary" 
+                            disabled={formData.quantite <=0 || formData.quantite > data.stock || (formData.quantite*1) % 1 !== 0}
+                            >Acheter</button>
+                        &nbsp;&nbsp;&nbsp;
+                        <span className="fw-bold fs-4 prix">{(data.prix * formData.quantite).toString().replace('.', ',')}€</span>
+                    </span>
+                </form>
+            </div>
         </div>
         
         : <span className='fst-italic text-body-secondary d-flex justify-content-center'>Indisponible pour le moment</span>}
